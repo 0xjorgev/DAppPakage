dcbb33192626fbaf1f0b0ac58710430e9f736fd1
Jorge Mendoza <jorgevmendoza@gmail.com>
2024-05-23 21:01:59 +0200

    Initial Commit

---
+++ Tests/DAppPackageTests/DAppPackageTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import DAppPackage
+
+final class DAppPackageTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
